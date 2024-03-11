36ea14227e2c8778375bf7b1fe455def2436cc6d
Akshit Trikha <akshittrikha01@gmail.com>
2024-03-11 21:42:26 +0530

    Add: functionality to send request to URL with request headers and query parameters

--- script.js
+++ script.js
@@ -5,16 +5,36 @@ import axios from 'axios'
 const queryParamsContainer = document.querySelector('[data-query-params]');
 const requestHeadersContainer = document.querySelector('[data-request-headers]');
 const keyValueTemplate = document.querySelector('[data-key-value-template]');
+const form = document.querySelector('[data-form]');
 
 queryParamsContainer.append(createKeyValuePair());
 requestHeadersContainer.append(createKeyValuePair());
 
-document.querySelector('[data-add-query-param-btn]').addEventListener('click', () => {
+document.querySelector('[data-add-query-param-btn]').addEventListener('click', (e) => {
+    e.preventDefault();
     queryParamsContainer.append(createKeyValuePair());
 })
 
-document.querySelector('[data-add-request-header-btn]').addEventListener('click', () => {
+document.querySelector('[data-add-request-header-btn]').addEventListener('click', (e) => {
+    e.preventDefault();
     requestHeadersContainer.append(createKeyValuePair());
+})
+
+form.addEventListener('submit', e => {
+    console.log("form submitted");
+    e.preventDefault();
+
+    console.log(keyValuePairsToObjects(queryParamsContainer));
+    console.log(keyValuePairsToObjects(requestHeadersContainer));
+
+     axios({
+        url: document.querySelector('[data-url]').value,
+        method: document.querySelector('[data-method]').value,
+        params: keyValuePairsToObjects(queryParamsContainer),
+        headers: keyValuePairsToObjects(requestHeadersContainer)
+     }).then((res) => {
+        console.log(res);
+     })
 })
 
 function createKeyValuePair() {
@@ -36,3 +56,14 @@ function createKeyValuePair() {
     return element;
 }
 
+
+function keyValuePairsToObjects(container) {
+    const pairs = container.querySelectorAll('[data-key-value-pair]');
+    return [...pairs].reduce((data, pair) => {
+        const key = pair.querySelector('[data-key]').value;
+        const value = pair.querySelector('[data-value]').value;
+
+        if(key === '') return data;
+        return {...data, [key]: value}
+    }, {})
+}
